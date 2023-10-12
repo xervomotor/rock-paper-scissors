633e1be41a6b3d5934b4eb3fea990e48fee982f3
xtang999 <xiaoli05tang@gmail.com>
2023-10-12 16:03:56 +1100

    first working version. ugly interface

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 
+// DOM elements
+const resultElem = document.getElementById('result');
+const playerScoreElem = document.getElementById('playerScore');
+const computerScoreElem = document.getElementById('computerScore');
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+// consts
 const choices = ['rock', 'paper', 'scissors'];
-const round = 3;
+const rotalRound = 3;
+let playerScore = 0, computerScore = 0, roundsPlayed = 0;
+
+rockBtn.addEventListener('click', () => game('rock'));
+paperBtn.addEventListener('click', () => game('paper'));
+scissorsBtn.addEventListener('click', () => game('scissors'));
+resetBtn.addEventListener('click', () => resetGame());
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -8,7 +24,7 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return 'tie game';
+        return 'tie';
     } else if ( (playerSelection === 'rock') && (computerSelection === 'scissors') ||
                 (playerSelection === 'scissors') && (computerSelection === 'paper') ||
                 (playerSelection === 'paper') && (computerSelection === 'rock')
@@ -19,32 +35,39 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
+function game(playerSelection) {
 
-    let playerScore = 0, computerScore = 0;
-
-    for (let i=0; i<round; i++) {
-
-        let playerSelection = prompt(`what is your selection? (round ${i+1})`).trim().toLowerCase();
-        if ( !choices.includes(playerSelection) ) {
-            playerSelection = prompt(`invalid, what is your selection? (round ${i+1})`).trim().toLowerCase();
-        }
+    if (roundsPlayed < rotalRound) {
 
         let computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-
-        
-        if (playRound(playerSelection, computerSelection) === 'player wins') {
+        let result = playRound(playerSelection, computerSelection);
+        resultElem.textContent = `${result}`;
+ 
+        if ( result === 'player wins') {
             playerScore++;
-        } else if (playRound(playerSelection, computerSelection) === 'computer wins') {
+        } else if ( result === 'computer wins') {
             computerScore++;
         }
 
+        playerScoreElem.textContent = playerScore;
+        computerScoreElem.textContent = computerScore;
+
+        roundsPlayed++;
+
+    } else {
+        resultElem.textContent = `you've played ${roundsPlayed} rounds. time to reset`;
     }
 
+
     console.log([playerScore, computerScore]);
-    return [playerScore, computerScore];
 
 }
 
-game();
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0; 
+    roundsPlayed = 0;
+    resultElem.textContent = `-`;
+    playerScoreElem.textContent = playerScore;
+    computerScoreElem.textContent = computerScore;
+}
